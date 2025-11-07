86ad1b08919cc0d6af29c7b36a9206aef1e53976
glks132 <aleqsandrefircxelani@gmail.com>
2025-11-07 08:46:22 -0800

    Add files via upload

---
+++ davaleba.py
@@ -0,0 +1,21 @@
+#debugging aris shecdomebis da errorebis chasworeba
+name = 14
+name = "ალექსანდრე"         #2
+print(name)
+
+pirveli = "gamarjoba"
+meore = " rogor xar "  #3
+mesame = pirveli + meore
+print(mesame)
+
+print(4564 % 4)
+print(9768 % 3)   #4
+print(16785 % 3)
+
+name = "saba"
+name = "giorgi"  #5
+print(name)
+
+
+num = 40
+print(num * 3)

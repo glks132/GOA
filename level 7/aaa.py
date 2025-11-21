4802779908419c96b149b2350850ebf511cdddf3
glks132 <aleqsandrefircxelani@gmail.com>
2025-11-21 09:27:35 -0800

    Add files via upload

---
+++ level 7/aaa.py
@@ -0,0 +1,17 @@
+asaki = int(input("შეიყვანეთ თქვენი ასაკი: ")) 
+print(asaki // 10)
+
+num1 = int(input("ნებისმიერი რიცხვი: " ))
+num2 = int(input("ნებისმიერი რიცხვი: " ))
+print(num1 * num2)
+
+age = input("your age: ")
+print(int(age))
+
+number = int(input("number:" ))
+number1 = int(input("number:" ))
+number2 = int(input("number:" ))
+print((number + number1 + number2)/3)
+
+დაბადების_წელი = int(input("დაბადების წელი"))
+print(2025 - დაბადების_წელი )

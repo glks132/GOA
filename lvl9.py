5647ca42f220dbfd7e9476b47a40372d682819dc
glks132 <aleqsandrefircxelani@gmail.com>
2025-12-05 02:25:11 -0800

    Add files via upload

---
+++ lvl9.py
@@ -0,0 +1,20 @@
+# srt() მონაცემი გადაყავს string ში
+# float() მონაცემი გადაყავს float ში
+#int() მონნაცემი გადაყავს integer ში
+
+mother = int(input("sheiyvanet asaki: "))
+father = int(input("sheiyvanet assaki: "))
+sister = int(input("sheiyvanet assaki: "))
+brother = int(input("sheiyvanet assaki: "))
+print(mother + 25)
+print(father + 25)
+print(sister + 25)
+print(brother + 25)
+
+
+name = input("tqveni saxeli: ")
+print(type(name))
+
+
+Asaki = int(input())
+print(str(Asaki))

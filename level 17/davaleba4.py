714b841a3bd1cf4cb4eb4c8beb46040d54ecbf4a
glks132 <aleqsandrefircxelani@gmail.com>
2026-01-30 19:20:06 +0400

    Add files via upload

---
+++ level 17/davaleba4.py
@@ -0,0 +1,9 @@
+# 4) for ციკლის გამოყენებით დაბეჭდე ყველა ელემენტი და მის გვერდით მისი ინდექსი.
+
+elements = ["pizza", 9, True, "idk", 2.8]
+
+
+index=0
+for i in elements:
+    print(i,index)
+    index+=1

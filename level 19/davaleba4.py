dcf45516e65b55d7a7acd42b4f96707ab637f3d8
glks132 <aleqsandrefircxelani@gmail.com>
2026-02-13 15:42:37 +0400

    Add files via upload

---
+++ level 19/davaleba4.py
@@ -0,0 +1,9 @@
+# 4)შემოატანინე მომხმარებელს გვარი და გამიტანე მისი გვარის პირველი 5 ასო შეტრიალებული
+
+surname=input("Enter your surnname: ")
+
+#gvinaze
+
+cvladi = (surname[5::-1])
+print(cvladi)
+
